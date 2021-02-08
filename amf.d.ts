1ab2f339f99fe7f8d67c48647627ba859ecb9c28
山村兼史 <kenshi@fes-total.com>
2021-02-08 21:03:48 +0900

    rollback

--- amf.d.ts
+++ amf.d.ts
@@ -1,15 +1,31 @@
 // Type definitions for amfjs
 // Project: https://github.com/kenshiYM/amfjs
-// Definitions by: kenshi Yamamura
+// Definitions by: kenshi yamamura
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 export declare namespace amf {
-    var classRegistry: any[];
+    var classes: any[];
+    var clients: Client[];
 
     export function registerClass(name: string, clazz: any);
-    export function init(endpoint: string, timeout?: number);
-    export function addHeader(name: string, value: string);
-    export function invoke(destination: string, source: string, operation: string, params: any, onResult: any, onStatus: any);
-}
+    export function getClient(destination: string): Client;
 
-export {amf};
+    export class Client {
+        constructor(destination: string, endpoint: string, timeout?: number);
+        setSessionId(value: string);
+        releaseQueue();
+        invoke<Response>(source: string, operation: string, params: any, block: boolean, nobatch: boolean): Promise<Response>;
+    }
+
+    export interface ResponseFactory {
+        new (code, message, detail, data, $scope): Response;
+    }
+    export var Response: ResponseFactory;
+    export interface Response {
+        $scope: any;
+        code: number;
+        message: string;
+        detail: any;
+        data: any;
+    }
+}
